1fd9011a23a98b02e06e63d09e711537ef7b16c4
Vladden1 <denisenkovv1@gmail.com>
2023-01-12 18:57:47 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,2 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const qwe = galleryItems;
+console.log(qwe);
+
+const galleryItem = document.querySelector(".gallery");
+galleryItem.classList = "gallery__item";
+
+console.log(galleryItem);
+
+for (const item of qwe) {
+  const galleryLink = document.createElement("a");
+  galleryLink.classList = "gallery__link";
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList = "gallery__image";
+
+  galleryImage.src = item.original;
+  galleryImage.alt = item.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+}
